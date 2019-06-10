5706f93002d2b0017b1b0f420f90d911ce113601
shilu <shilu@tusvn.comm>
2019-06-10 18:59:00 +0800

    数据概览地图调用方式wms

--- src/views/dataMonitor/components/map.vue
+++ src/views/dataMonitor/components/map.vue
@@ -24,35 +24,6 @@ export default {
                 params:{'LAYERS': 'shanghai_qcc:gd_road_centerline',VERSION:'1.1.0'}
             })
             wms.setMap(this.AMap)
-        },
-        drawLine(attr) {
-            let _this = this;
-            new AMap.Polyline({
-                map: _this.AMap,
-                path: _this.linePath[attr],
-                strokeColor: "#6fb07d",
-                strokeWeight: 1,
-                strokeStyle: "solid",
-                lineJoin: 'round',
-                lineCap: 'round',
-                zIndex: 50
-            });
-            // 缩放地图到合适的视野级别
-            this.AMap.setFitView();
-            this.$parent.$parent.changeCenterPoint = this.AMap.getCenter();
-        },
-        drawPolygon(attr) {
-            let _this = this,
-                _style = this.polygonStyle[Number(attr)%3],
-                _defaultOption = {
-                    map: _this.AMap,
-                    path: _this.polygonPoint[attr],
-                    zIndex: 50
-                },
-                _option = Object.assign(_defaultOption, _style);
-                new AMap.Polygon(_option);
-            // 缩放地图到合适的视野级别
-            this.AMap.setFitView();
         }
     }
 }
