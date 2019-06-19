04f63cf787b66cd20ab5a4c4f63aac695e342cfd
邱丽娜 <qiulina@tusvn.com>
2019-06-19 15:10:59 +0800

    望京地址配置

--- static/config/config.js
+++ static/config/config.js
@@ -1,15 +1,16 @@
 window.cfg = {
-    url: 'http://10.0.1.57:9093/monPlatApp/',
+    /*url: 'http://10.0.1.57:9093/monPlatApp/',
     websocketUrl:'ws://10.0.1.57:9982/mon',
     // socketUrl:'ws://10.0.1.57:9999/ws',
-    socketUrl:'ws://192.168.1.51:9999/ws',
+    socketUrl:'ws://192.168.1.51:9999/ws',*/
     roadUrl:'http://120.133.21.14:28080/rCUDataApp/', //临时演示地址
     dlUrl: 'http://10.0.1.22:8085/', //迪路
     /*testUrl:'http://10.0.1.57:9092/icvShadowApp/wstester.html',*/
+    /*testUrl:'http://172.17.1.13:9092/icvShadowApp/ws.html',*/
     version: 1.0,       // 版本号
 
     //望京地址
-    /*url: 'http://172.17.1.13:9093/monPlatApp/', //监控平台
-    websocketUrl:'ws://172.17.1.13:9982/ws',  //监控
-    socketUrl:'ws://172.17.1.13:9999/ws'  //影子系统*/
+    url: 'http://172.17.1.13:9093/monPlatApp/', //监控平台
+    websocketUrl:'ws://172.17.1.13:9982/mon',  //监控
+    socketUrl:'ws://172.17.1.13:9999/ws'  //影子系统
 }
