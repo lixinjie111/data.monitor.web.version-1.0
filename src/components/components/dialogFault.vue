cb96f4d218b1ce875fce597372de959365e93d3a
shilu <shilu@tusvn.comm>
2019-07-15 11:53:15 +0800

    预警和故障布局

---
+++ src/components/components/dialogFault.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="c-dialog-wrapper">
+        <div class="c-dialog-container">
+            <div class="c-dialog-header">
+                <span class="c-dialog-title">故障信息</span>
+                <i class="c-dialog-close" @click="closeDialog"></i>
+            </div>
+            <div class="c-dialog-content">
+                <div class="c-scroll-wrap">
+                    <ul class="c-scroll-inner">
+                        故障
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "DialogFault",
+    props: {
+        
+    },
+    data() {
+        return {
+           
+        }
+    },
+    mounted(){
+    },
+    methods: {
+        closeDialog() {
+            this.$emit("closeDialogFault");
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+@import '@/assets/scss/theme.scss';
+
+</style>
