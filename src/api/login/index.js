a05fdf1a04d8576f64ce95b224816d99510d2827
shilu <shilu@tusvn.comm>
2019-08-20 18:19:14 +0800

    优化

--- src/api/login/index.js
+++ src/api/login/index.js
@@ -5,11 +5,9 @@ import {HTTPURL} from '../requestUrl';
  * 登录
  */
 export const requestLogin = params => { return axios.post(`${HTTPURL}admin/sys/user/login`, params).then(res => res.data); };
-//export const requestLogin = params => { return axios.post(`http://120.133.21.14:9090/operateApp/sys/user/login`, params).then(res => res.data); };
 /**
  * 退出登录
  */
 export const requestLogout = params => { return axios.get(`${HTTPURL}admin/sys/user/logout`, {params: params}).then(res => res.data); };
-//export const requestLogout = params => { return axios.get(`http://120.133.21.14:9090/operateApp/sys/user/logout`, {params: params}).then(res => res.data); };
 
 
--- static/config/config.js
+++ static/config/config.js
@@ -17,6 +17,8 @@ window.config = {
     // socketUrl:'ws://172.17.1.13:49999/ws',  //影子系统
     // dlUrl: 'http://10.0.1.22:8085/', //迪路
     // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
+    // roadUrl:'http://172.17.1.13:28080/rCUDataApp/', //临时演示地址
+    // monitorWebsocket:'ws://172.17.1.13:29998/ws',
 
     //外网
     url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
@@ -24,6 +26,8 @@ window.config = {
     socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
     dlUrl: 'http://113.208.118.62:8085/', //迪路
     dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
+    roadUrl:'http://120.133.21.14:28080/rCUDataApp/', //临时演示地址
+    monitorWebsocket:'ws://120.133.21.14:29998/ws',
 
     version: 1.0,       // 版本号
 }
