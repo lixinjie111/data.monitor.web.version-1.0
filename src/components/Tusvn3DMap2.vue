6fbff01413be81b7380d22d4bd133daf78ccc688
yuyifang <yu.yf@foxmail.com>
2019-08-29 19:26:09 +0800

    no message

--- src/components/Tusvn3DMap2.vue
+++ src/components/Tusvn3DMap2.vue
@@ -299,7 +299,7 @@ export default {
                     for(let m = 0;m<this.cacheModelNum;m++)
                     {
                         //车
-                        var geoBox1 = new THREE.BoxBufferGeometry(1.7, 4.6, 1.4);
+                        var geoBox1 = new THREE.BoxBufferGeometry(1.6, 3.8,1.4);
                         var model1 = new THREE.Mesh( geoBox1, this.matStdObjects );
                         model1.position.set( 0, 0, 0 );
                         model1.rotation.set( this.pitch,this.yaw,this.roll );
@@ -345,6 +345,10 @@ export default {
                         person.position.x = 0;
                         person.position.y = 0;
                         person.position.z = 0;
+
+                        var text1 = this.deviceModels[deviceid].texts[m];
+                        text1.setPositon([0,0,0]);
+                        text1.update();
                     }
                 }
 
