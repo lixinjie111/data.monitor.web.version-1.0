c55184b74e3e3ac86d44eb2c46df9ec6c411f3c0
邱丽娜 <qiulina@tusvn.com>
2019-09-03 14:51:16 +0800

    Merge branch 'dev' of http://git.tusvn.net/shilu/data.monitor.web.version-1.0 into dev
    # Conflicts:
    #	package-lock.json
    #	static/config/config.js

--- static/config/config.js
+++ static/config/config.js
@@ -22,13 +22,13 @@ window.config = {
   // http://172.17.1.13:9093/monPlatApp/ws.html
 
     //外网
-    // url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
-    // websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
-    // socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
-    // dlUrl: 'http://113.208.118.62:8085/', //迪路
-    // dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
-    // roadUrl:'http://120.133.21.14:28080/rCUDataApp/', //临时演示地址
-    // monitorWebsocket:'ws://120.133.21.14:29998/ws',
+    url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
+    websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
+    socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
+    dlUrl: 'http://113.208.118.62:8085/', //迪路
+    dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
+    roadUrl:'http://120.133.21.14:28080/rCUDataApp/', //临时演示地址
+    monitorWebsocket:'ws://120.133.21.14:29998/ws',
 
     //长沙
     // url: 'http://111.23.140.59:9093/monPlatApp/', //监控平台
