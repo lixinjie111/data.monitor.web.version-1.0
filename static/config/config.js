b0f5dba448f3527251e16e7e383a84bf4cd37ff5
lixinjie <15110074982@139.com>
2019-09-03 14:48:19 +0800

    新建长沙项目

--- static/config/config.js
+++ static/config/config.js
@@ -1,42 +1,12 @@
 window.config = {
-    /*url: 'http://10.0.1.57:9093/monPlatApp/',
-    websocketUrl:'ws://10.0.1.57:9982/mon',
-    // socketUrl:'ws://10.0.1.57:9999/ws',
-    socketUrl:'ws://192.168.1.51:9999/ws',*/
-    /*testUrl:'http://10.0.1.57:9092/icvShadowApp/wstester.html',*/
-    /*testUrl:'http://172.17.1.13:9092/icvShadowApp/ws.html',*/
-    /*url: 'http://192.168.1.57:8080/', //监控平台
-    socketUrl:'ws://192.168.1.57:9999/ws',  //监控
-    swaggerUrl:http://172.17.1.13:9093/monPlatApp/swagger-ui.html
-    // roadUrl:'http://172.17.1.13:28080/rCUDataApp/', //临时演示地址
-    */
-
-    //望京地址  内网
-    url: 'http://172.17.1.13:9093/monPlatApp/', //监控平台
-    websocketUrl:'ws://172.17.1.13:9982/mon',  //监控
-    socketUrl:'ws://172.17.1.13:9999/ws',  //影子系统
-    dlUrl: 'http://10.0.1.22:8085/', //迪路
-    dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
-    roadUrl:'http://172.17.1.13:28080/rCUDataApp/', //临时演示地址
-    monitorWebsocket:'ws://172.17.1.13:29998/ws',
-
-    //外网
-    // url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
-    // websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
-    // socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
-    // dlUrl: 'http://113.208.118.62:8085/', //迪路
-    // dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
-    // roadUrl:'http://120.133.21.14:28080/rCUDataApp/', //临时演示地址
-    // monitorWebsocket:'ws://120.133.21.14:29998/ws',
-
     //长沙
-    // url: 'http://111.23.140.59:9093/monPlatApp/', //监控平台
-    // websocketUrl:'ws://111.23.140.59:9982/mon',  //监控
-    // socketUrl:'ws://111.23.140.59:9999/ws',  //影子系统
-    // dlUrl: 'http://111.23.140.59:8085/', //迪路
-    // dlWmsUrl: 'http://111.23.140.59:8080/', //迪路
-    // roadUrl:'http://111.23.140.59:28080/rCUDataApp/', //临时演示地址
-    // monitorWebsocket:'ws://111.23.140.59:29998/ws',
+       url: 'http://111.23.140.59:9093/monPlatApp/', //监控平台
+       websocketUrl:'ws://111.23.140.59:9982/mon',  //监控
+       socketUrl:'ws://111.23.140.59:9999/ws',  //影子系统
+       dlUrl: 'http://111.23.140.59:8085/', //迪路
+       dlWmsUrl: 'http://111.23.140.59:8080/', //迪路
+       roadUrl:'http://111.23.140.59:28080/rCUDataApp/', //临时演示地址
+       monitorWebsocket:'ws://111.23.140.59:29998/ws',
 
     version: 1.0,       // 版本号
 }
