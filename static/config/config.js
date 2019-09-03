59db1f0b479c740fcb2c8a7753b3e23872a3f9bd
fuyuxuan <fuyuxuan@tusvn.com>
2019-09-03 16:26:07 +0800

    1

--- static/config/config.js
+++ static/config/config.js
@@ -1,21 +1,21 @@
 window.config = {
     //望京地址  内网
-    url: 'http://172.17.1.16:9093/monPlatApp/', //监控平台
-    websocketUrl:'ws://172.17.1.16:9982/mon',  //监控
-    socketUrl:'ws://172.17.1.16:9999/ws',  //影子系统
-    dlUrl: 'http://10.0.1.22:8085/', //迪路
-    dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
-    roadUrl:'http://172.17.1.16:28080/rCUDataApp/', //临时演示地址
-    monitorWebsocket:'ws://172.17.1.16:29998/ws',
+    // url: 'http://172.17.1.16:9093/monPlatApp/', //监控平台
+    // websocketUrl:'ws://172.17.1.16:9982/mon',  //监控
+    // socketUrl:'ws://172.17.1.16:9999/ws',  //影子系统
+    // dlUrl: 'http://10.0.1.22:8085/', //迪路
+    // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
+    // roadUrl:'http://172.17.1.16:28080/rCUDataApp/', //临时演示地址
+    // monitorWebsocket:'ws://172.17.1.16:29998/ws',
 
     //外网
-    // url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
-    // websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
-    // socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
-    // dlUrl: 'http://113.208.118.62:8085/', //迪路
-    // dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
-    // roadUrl:'http://120.133.21.14:28080/rCUDataApp/', //临时演示地址
-    // monitorWebsocket:'ws://120.133.21.14:29998/ws',
+    url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
+    websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
+    socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
+    dlUrl: 'http://113.208.118.62:8085/', //迪路
+    dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
+    roadUrl:'http://120.133.21.14:28080/rCUDataApp/', //临时演示地址
+    monitorWebsocket:'ws://120.133.21.14:29998/ws',
 
     version: 1.0,       // 版本号
 }
