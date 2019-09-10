2059a3d6a1e2b4b5ba6d8eed1368b70fdcb5cc7c
shilu <shilu@tusvn.comm>
2019-09-10 15:31:22 +0800

    优化

--- src/views/dataMonitor/components/map.vue
+++ src/views/dataMonitor/components/map.vue
@@ -2,7 +2,6 @@
     <div class="c-view-main" :id="id"></div>
 </template>
 <script>
-import { getRoadCenterIds, getRoadCenterPoints } from '@/api/dataMonitor'
 export default {
     name: "MapContainer",
     data () {
