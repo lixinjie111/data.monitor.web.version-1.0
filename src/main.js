945bb2adc92fcd90370742b21c32076a69eeaea6
lixinjie <15110074982@139.com>
2019-09-11 14:55:52 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ import 'nprogress/nprogress.css'
 import VideoPlayer  from 'vue-video-player'
 import 'video.js/dist/video-js.css'
 import 'vue-video-player/src/custom-theme.css'
+import "@/assets/scss/video-reset.scss";
 import 'videojs-flash'
 
 // import $ from 'jquery'
