127d200b10585bd0c0325c23d5cf946837ccba3c
shilu <shilu@tusvn.comm>
2019-09-23 12:55:59 +0800

    设置默认用户

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -65,24 +65,25 @@ export default {
         }
     },
     created() {
-        let _data = localStorage.getItem("yk-token");
-        if(_data) {
-            let _dataObj = JSON.parse(_data),
-                _delayTime = 1000 * 60 * 60 * 24;
-            if (new Date().getTime() - _dataObj.time > _delayTime) {
-                console.log('信息已过期');
-                this.removeStorage();
-            }else{
-                // 直接调用登录接口
-                let _params = {
-                    token: _dataObj.data,
-                    platform: this.$store.state.admin.platform
-                };
-                this.loginFunc(_params);
-            }
-        }else {
-            this.removeStorage();
-        }
+        this.visibleFlag = true;
+        // let _data = localStorage.getItem("yk-token");
+        // if(_data) {
+        //     let _dataObj = JSON.parse(_data),
+        //         _delayTime = 1000 * 60 * 60 * 24;
+        //     if (new Date().getTime() - _dataObj.time > _delayTime) {
+        //         console.log('信息已过期');
+        //         this.removeStorage();
+        //     }else{
+        //         // 直接调用登录接口
+        //         let _params = {
+        //             token: _dataObj.data,
+        //             platform: this.$store.state.admin.platform
+        //         };
+        //         this.loginFunc(_params);
+        //     }
+        // }else {
+        //     this.removeStorage();
+        // }
     },
  	mounted() {
        this.getCookie();
@@ -92,13 +93,22 @@ export default {
         handleLogin() {
             this.$refs.loginForm.validate(valid => {
                 if (valid) {
-                    this.loading = true;
-                    if (this.checked == true) {
-                        this.setCookie(this.loginForm.userNo, this.loginForm.password, 7);
+                    if(this.loginForm.userNo == 'qcctest1') {
+                        this.loading = true;
+                        if (this.checked == true) {
+                            this.setCookie(this.loginForm.userNo, this.loginForm.password, 7);
+                        }else {
+                            this.clearCookie();
+                        }
+                        this.loginFunc(this.loginForm); 
                     }else {
-                        this.clearCookie();
-                    }
-                    this.loginFunc(this.loginForm);     
+                        this.$message({
+                            type: 'error',
+                            duration: '1500',
+                            message: '该用户没有权限，请重试！',
+                            showClose: true
+                        });
+                    }    
                 } else {
                     this.loading = false;
                     return false;
@@ -149,7 +159,7 @@ export default {
                 //this.$refs.loginForm.resetFields();
                 this.loginForm.userNo = "";
                 this.loginForm.password = "";
-                this.checked = false;
+                this.checked = true;
             }
         },
         //清除cookie
