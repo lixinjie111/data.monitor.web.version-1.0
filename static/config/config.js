2f2d261a9ec170e7222201283b9aba8b8f692fe1
fuyuxuan <fuyuxuan@tusvn.com>
2019-09-23 19:00:56 +0800

    modify config。js

--- static/config/config.js
+++ static/config/config.js
@@ -1,7 +1,6 @@
 window.config = {
-
     //雄安 外网地址
-    url: 'http://144.7.122.89:9093/monPlatApp/', //监控平台
+    url: 'http://144.7.122.19:9093/monPlatApp/', //监控平台
     operateUrl: 'http://144.7.122.89:9090/operateApp/',	//运营平台
     websocketUrl:'ws://144.7.122.19:9982/mon',  //监控
     socketUrl:'ws://144.7.122.84:9999/ws',  //影子系统
@@ -12,12 +11,10 @@ window.config = {
 }
 //地图额外配置项添加
 window.mapOption = {
-	// center: [121.551976, 31.276054],	//上海-高德地图坐标点
-	// center: [113.07876,28.255618],	//长沙-高德地图坐标点
     mapStyleEmpty: "amap://styles/bc5a63d154ee0a5221a1ee7197607a00", // 纯灰色背景地图 路口
-    defaultCenterPoint :  [121.262939,31.245149],   // 上海默认中心点 --- 高德坐标点
-    centerPoint :  [121.17265957261286,31.284096076877844],     // 数据概览 中心点位置(获取地理位置计算天气) --- 高德坐标点
-    singlePoint:[121.18653381418872,31.274421462567677],    // 单车监控 大地图中心点 --- 高德坐标点
+    defaultCenterPoint :  [115.912987, 39.049487],   // 雄安默认中心点 --- 高德坐标点
+    centerPoint :  [115.912987, 39.049487],     // 数据概览 中心点位置(获取地理位置计算天气) --- 高德坐标点
+    singlePoint:[115.912987, 39.049487],    // 单车监控 大地图中心点 --- 高德坐标点
 };
 window.defaultMapOption = {
 	center: window.mapOption.defaultCenterPoint,
@@ -26,18 +23,6 @@ window.defaultMapOption = {
 	rotateEnable: true,
 	mapStyle: "amap://styles/3312a5b0f7d3e828edc4b2f523ba76d8"
 };
-
-// 调用
-// this.distanceMap = new AMap.Map('map-container', window.defaultMapOption);
-// let _option = Object.assign(
-// 	{},
-// 	window.defaultMapOption,
-// 	{
-// 		mapStyle: window.mapOption.mapStyleEmpty
-// 	}
-// );
-// this.distanceMap = new AMap.Map('map-container', _option);
-
 //路网配置参数
 window.dlWmsOption = {
     'LAYERS_dlzc': 'shanghai_qcc:gd_dlzc',
@@ -53,40 +38,29 @@ window.dlWmsDefaultOption = {
     tileSize: 256,
     params:{}
 }
-//	调用
-// let _optionWms = Object.assign(
-// 	{},
-// 	window.dlWmsDefaultOption,
-// 	{
-// 		params:{'LAYERS': window.dlWmsOption.LAYERS_centerline,'STYLES': window.dlWmsOption.STYLES, 'VERSION': window.dlWmsOption.VERSION}
-// 	}
-// );
-// this.wms = new AMap.TileLayer.WMS(_optionWms);
-// this.distanceMap.add(this.wms);
-
 // 3D地图范围默认参数配置
 window.mapParam={
-    minX:325295.155400,
-    minY:3461941.703700,
-    minZ:50,
-    maxX:326681.125700,
-    maxY:3462723.022400,
-    maxZ:80,
+    minX:0,
+    minY:0,
+    minZ:0,
+    maxX:0,
+    maxY:0,
+    maxZ:0,
     background:"black"
 };
 
 window.defaultMapParam = {
-    x:442456.0905737311,
-    y:4427223.965503679,
-    z:60.22107514430958,
-    radius:31.40011218302981,
-    pitch:-0.6747685185185206,
-    yaw:-0.7853942219746017
+    x:0,
+    y:0,
+    z:0,
+    radius:0,
+    pitch:0,
+    yaw:0
 }
 
 window.circleParam = {
     redius:600.0,
-    center:[325535.061,3462402.816,13.618],
+    center:[0,0,0],
     color:"#ffffff",
     outline:false,
     outlineColor:"#ff0000",
