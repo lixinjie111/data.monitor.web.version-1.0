94042c539b9c716717214ae6c63509f19878cf4c
lixinjie <15110074982@139.com>
2019-12-19 11:42:21 +0800

    调试

--- static/config/config.js
+++ static/config/config.js
@@ -10,11 +10,11 @@ window.config = {
     // dlWmsUrl: 'http://117.114.144.227:8080/', //迪路
 
     //上海正式环境  外网
-    staticUrl: "http://116.236.72.204:49090/", //静态文件路径
-    url: 'http://116.236.72.206:49093/monPlatApp/', //监控平台
-    operateUrl: 'http://116.236.72.204:49090/operateApp/', //运营平台
-    websocketUrl:'ws://116.236.72.206:49982/mon',  //监控
-    socketUrl:'ws://116.236.72.205:49999/ws',  //影子系统
+    staticUrl: "http://116.236.72.204:59090/", //静态文件路径
+    url: 'http://116.236.72.204:59093/monPlatApp/', //监控平台
+    operateUrl: 'http://116.236.72.204:59090/operateApp/', //运营平台
+    websocketUrl:'ws://116.236.72.204:59982/mon',  //监控
+    socketUrl:'ws://116.236.72.204:59999/ws',  //影子系统
     dlUrl: 'http://116.236.72.204:48085/', //迪路
     dlWmsUrl: 'http://116.236.72.204:48080/', //迪路
 
