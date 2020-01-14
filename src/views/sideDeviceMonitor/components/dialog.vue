a5a0e093835449a06a25366d5ab8aa104e5aea73
fuyuxuan <fuyuxuan@tusvn.com>
2020-01-14 16:38:56 +0800

    modify  单车 echarts 图标样式

--- src/views/sideDeviceMonitor/components/dialog.vue
+++ src/views/sideDeviceMonitor/components/dialog.vue
@@ -260,8 +260,8 @@ export default {
     }
   },
   beforeCreate(){
-    // this.iframeSrc = window.config.staticUrl+'cesium-map/modules/monPlatform/index.html';      
-    this.iframeSrc =  'http://127.0.0.1:8082/modules/monPlatform/index.html';       
+    this.iframeSrc = window.config.staticUrl+'cesium-map/modules/monPlatform/index.html';      
+    // this.iframeSrc =  'http://127.0.0.1:8082/modules/monPlatform/index.html';       
   },
   mounted() {
     this.selectAddr = this.treeItem.path.split("|");
